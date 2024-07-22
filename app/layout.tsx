e49ce341a633a3240fb20a5ea1691999b41d1a75
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-22 20:28:05 +0530

    Add globals.css and tailwind.config.ts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import "./globals.css";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Your go-to-collaborative editor for live docs.",
+};
 
 export default function RootLayout({
   children,
@@ -15,14 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
+        className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
       >
-        ...
+        {children}
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-import type { Config } from "tailwindcss"
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import type { Config } from "tailwindcss";
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config = {
   darkMode: ["class"],
@@ -16,9 +17,29 @@ const config = {
       padding: "2rem",
       screens: {
         "2xl": "1400px",
+        xs: "360px",
       },
     },
     extend: {
+      colors: {
+        blue: {
+          100: "#B4C6EE",
+          400: "#417BFF",
+          500: "#3371FF",
+        },
+        red: {
+          400: "#DD4F56",
+          500: "#DC4349",
+        },
+        dark: {
+          100: "#09111F",
+          200: "#0B1527",
+          300: "#0F1C34",
+          350: "#12213B",
+          400: "#27344D",
+          500: "#2E3D5B",
+        },
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
@@ -31,6 +52,10 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+      },
+      backgroundImage: {
+        doc: "url(/assets/images/doc.png)",
+        modal: "url(/assets/images/modal.png)",
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
