9b8ca5275c73079e74915ad74b216e67592d062f
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-24 15:22:27 +0530

    Add Header: initial jsx elements

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,14 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
-      Document
+      <Header>
+        <p className='text-orange-300'>Test</p>
+      </Header>
+      <Editor />
     </div>
   )
 }
