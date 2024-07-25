345e2115931401852c2f907a555fc532ebdc10d2
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-25 23:57:43 +0530

    Add room for collaboration

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,10 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 import React from "react";
 
 const Document = () => {
   return (
     <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title mx-2">Share</p>
-        </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </Header>
-      <Editor />
+      
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,13 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider
-    appearance={{
-      baseTheme: dark,
-      variables: {
-        colorPrimary: "#3371FF",
-        fontSize: "16px"
-      }
-    }}
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+      }}
     >
       <html lang="en" suppressHydrationWarning>
         <body
@@ -38,7 +39,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
