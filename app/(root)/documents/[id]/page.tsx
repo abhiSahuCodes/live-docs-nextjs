2cdd66d85ce49be0e821b3f61779a78d57f494ce
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-26 22:57:03 +0530

    Add collaborative room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,12 @@
 
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 import React from "react";
 
 const Document = () => {
   return (
-    <div>
-      
-    </div>
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom />
+    </main>
   );
 };
 
--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -1,11 +1,24 @@
-'use client';
+"use client";
 
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { createDocument } from "@/lib/actions/room.actions";
+import { useRouter } from "next/navigation";
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
-  const addDocumentHandler = async () => {};
+  const router = useRouter();
+  const addDocumentHandler = async () => {
+    try {
+      const room = await createDocument({ userId, email });
+
+      if (room) {
+        router.push(`/documents/${room.id}`);
+      }
+    } catch (error) {
+      console.log(`Error happend while creating a room: ${error}`);
+    }
+  };
 
   return (
     <Button
