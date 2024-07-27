e2ae9e5025e5fe5f41c87415efc8962a52c20cd0
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-27 22:40:09 +0530

    Add change in Provider and add user actions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -8,10 +8,18 @@ import {
 } from "@liveblocks/react/suspense";
 
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+
+        return users;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
