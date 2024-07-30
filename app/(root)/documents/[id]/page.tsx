389baf981b58e6c1d311ef879d434e37e073ac7d
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-30 11:45:09 +0530

    Add live features with comment feature

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { getDocument } from "@/lib/actions/room.actions";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -16,12 +17,24 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
 
   if (!room) redirect('/');
 
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({ userIds });
+
+  const usersData = users.map((user: User) => ({
+    ...user,
+    userType: room.usersAccesses[user.email?.includes('room:write')
+      ? 'editor'
+      : 'viewer'
+    ],
+  }))
+
+  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer';
 
   return (
     <main className="flex w-full flex-col items-center">
       <CollaborativeRoom
         roomId={id}
-        roomMetadata={room.metadata} users={[]} currentUserType={"creator"}       />
+        roomMetadata={room.metadata} users={usersData} currentUserType={currentUserType}       />
     </main>
   );
 };
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -15,8 +15,9 @@ import Loader from "./Loader";
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
+  users,
+  currentUserType
 }: CollaborativeRoomProps) => {
-  const currentUserType = "editor";
 
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
@@ -116,7 +117,7 @@ const CollaborativeRoom = ({
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType} />
         </div>
       </ClientSideSuspense>
     </RoomProvider>
