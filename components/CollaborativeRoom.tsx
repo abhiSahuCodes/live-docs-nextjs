91599896a35964a749a1673b077da9fbe9318cc5
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-31 23:03:06 +0530

    Add share feature

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -11,6 +11,7 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import ShareModal from "./ShareModal";
 
 const CollaborativeRoom = ({
   roomId,
@@ -109,6 +110,12 @@ const CollaborativeRoom = ({
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollborators />
+              <ShareModal 
+              roomId={roomId}
+              collaborators={users}
+              creatorId={roomMetadata.creatorId}
+              currentUserType={currentUserType}
+              />
               <SignedOut>
                 <SignInButton />
               </SignedOut>
