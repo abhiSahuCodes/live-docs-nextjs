0a60f353fb0c20964dd479ef2f7159c39289aeb0
Abhishek Sahu <abhishek963sahu@gmail.com>
2024-07-31 14:42:40 +0530

    Add user mention

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -8,15 +8,26 @@ import {
 } from "@liveblocks/react/suspense";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user: clerkUser } = useUser();
+  
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
 
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const users = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
         return users;
       }}
     >
